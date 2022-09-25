a5aa37c195060cdded464fc3d48f4da066d45987
Lvcius <90807618+Lvcius@users.noreply.github.com>
2022-09-25 20:13:05 +1300

    display date at top and resizing
    Set the textview at the top of the screen to display the date (will be used in conjunction with a Week Calendar later), resized UI elements to be a bit smaller, allowing for more information to be displayed.

--- app/src/main/java/com/example/dgtapp/MainActivity.kt
+++ app/src/main/java/com/example/dgtapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.dgtapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // gets time from Calendar and formats it
+        val c: Calendar = Calendar.getInstance()
+        val df = SimpleDateFormat("MMM d yyyy")
+        val formattedDate: String = df.format(c.time)
+        // sets the text in the TextView to the date (using the format above)
+        val textView = findViewById<View>(R.id.text_date_display) as TextView
+        textView.text = formattedDate
     }
 }
