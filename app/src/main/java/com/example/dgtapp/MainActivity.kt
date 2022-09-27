ef69587068f972e46fe852a78656c8897946fc23
Lvcius <aidanambion.not@gmail.com>
2022-09-27 15:12:07 +1300

    27.09 - set up for todo list (at school))
    - Imported icons for todo list
    - started setting up basic structure

--- app/src/main/java/com/example/dgtapp/MainActivity.kt
+++ app/src/main/java/com/example/dgtapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dgtapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,7 +20,21 @@ class MainActivity : AppCompatActivity() {
         val df = SimpleDateFormat("MMM d yyyy")
         val formattedDate: String = df.format(c.time)
         // sets the text in the TextView to the date (using the format above)
-        val textView = findViewById<View>(R.id.text_date_display) as TextView
+        val textView = findViewById<View>(R.id.tvDate) as TextView
         textView.text = formattedDate
+
+        //show/hide edittext and submit button for adding new todo item
+        val etToDo = findViewById<>(R.id.etToDo);
+        etToDo.isvisible = false
+
+        fun showHide(view:View) {
+            view.visibility = if (view.visibility == View.VISIBLE){
+                View.INVISIBLE
+            } else{
+                View.VISIBLE
+            }
+        }
+
+
     }
 }
